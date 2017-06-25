2c20135083532b90228c6a06ab045f4e808bd6e5
Yuusaku Miyazaki <toumin.m7@gmail.com>
2017-06-25 09:47:42 +0900

    Migrate and seed.

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Use Faker
+        // https://github.com/fzaninotto/Faker
+        $faker = Faker\Factory::create('ja_JP');
+        for ($i = 0; $i < 40; $i++) {
+            DB::table('posts')->insert([
+                'title' => $faker->text(20),
+                'body' => $faker->text(200),
+                'created_at' => $faker->dateTime(),
+                'updated_at' => $faker->dateTime(),
+                'user_id' => $faker->numberBetween(1, 20),
+            ]);
+        }
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Use Faker
+        // https://github.com/fzaninotto/Faker
+        $faker = Faker\Factory::create('ja_JP');
+        DB::table('users')->insert([
+            'name' => 'sutara79',
+            'email' => 'toumin.m7@gmail.com',
+            'password' => bcrypt('1234'),
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'foo1',
+            'email' => 'foo1@foo.com',
+            'password' => bcrypt('1234'),
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+        ]);
+        for ($i = 0; $i < 18; $i++) {
+            DB::table('users')->insert([
+                'name' => $faker->unique()->userName(),
+                'email' => $faker->unique()->email(),
+                'password' => bcrypt('1234'),
+                'created_at' => $faker->dateTime(),
+                'updated_at' => $faker->dateTime(),
+            ]);
+        }
+    }
+}
